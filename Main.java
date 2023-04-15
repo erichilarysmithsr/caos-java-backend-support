7d365e65552bce26622119bf05b2ad75cf3de63e
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-15 13:31:34 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.aphidb;
+
+import java.lang.Object;
+import com.squareup.okhttp.*;
+
+class Main {
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\n    \"collection\":\"PIRDDIIQFoodOntoMyPlate\",\n    \"database\":\"CO2REMEDi\",\n    \"dataSource\":\"CAOS\",\n    \"projection\": {\"_id\": 1}\n\n}");
+        Request request = new Request.Builder()
+          .url("https://data.mongodb-api.com/app/data-hgnct/endpoint/data/v1/action/findOne")
+          .method("POST", body)
+          .addHeader("Content-Type", "application/json")
+          .addHeader("Access-Control-Request-Headers", "*")
+          .addHeader("api-key", "<API_KEY>")
+          .build();
+        Response response = client.newCall(request).execute();
+    }
+}
